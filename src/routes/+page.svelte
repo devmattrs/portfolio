899a96458b29cb5289a39eafaf594730b9d004e7
Matt Schroder <matt@strukt.io>
2025-02-11 16:55:16 -0600

    nice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import Hero from './components/hero.svelte';
-	import Projects from './components/projects.svelte';
+	import Projects from './components/Projects.svelte';
 	import Skills from './components/skills.svelte';
-	import Companies from './components/companies.svelte';
 </script>
 
 <main class="relative z-10 mx-auto mt-12 min-h-screen max-w-3xl px-6 py-12 md:px-6">

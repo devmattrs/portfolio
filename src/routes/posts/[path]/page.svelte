93072587f5355eb5df20cae87dfff622f312dd45
Matt Schroder <matt@strukt.io>
2025-02-20 20:53:36 -0600

    removed blog posts

--- src/routes/posts/[path]/page.svelte
+++ src/routes/posts/[path]/page.svelte
@@ -4,13 +4,12 @@
 	import { compile } from 'mdsvex';
 
 	let { data }: { data: PageData } = $props();
-
 </script>
 
 <main class="relative mx-auto mt-12 min-h-screen max-w-3xl px-6 py-12 md:px-6">
-	{#await data.post.content}
-		<div>Loading...</div>
-	{:then data.post.content}
-		{@render data.post.content.component()}
-	{/await}
+	<!-- {#await data.post.content} -->
+	<!-- 	<div>Loading...</div> -->
+	<!-- {:then data.post.content} -->
+	<!-- 	{@render data.post.content.component()} -->
+	<!-- {/await} -->
 </main>
--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(),
+	preprocess: [
+		vitePreprocess()
 		// mdsvex({
 		// 	extensions: ['.svx', '.md'],
 		// })
 	],
 
-	extensions: ['.svelte', '.svx', '.md'],
+	// extensions: ['.svelte', '.svx', '.md'],
+	extensions: ['.svelte'],
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -23,8 +25,7 @@ const config = {
 		$components: './src/components',
 		$lib: './src/lib',
 		$posts: './src/posts'
-	},
-
+	}
 };
 
 export default config;
